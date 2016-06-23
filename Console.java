1e4fa9eae7d4ac30002253259c478e8602473a9d
Juan Neri <Juanjo.neri@gmail.com>
2016-06-23 15:52:59 -0300

    added methos for painting various pixels at a time

--- Console.java
+++ Console.java
@@ -1,40 +1,54 @@
 public class Console{
-    
+//This class uses a matrix to paint and adds functionality to paint it in cmd
+
     //CLASS LEVEL VARIABLES ------------
     private TheMatrix matrix;
-    
+    private static final int SLEEP_TIME = 500; //milliseconds
+
     //CONSTRUCTORS ---------------------
     public Console(){
         this(10,10);
     }
 
-    public Console(int xLength,int yLength) throws IllegalArgumentException{      
+    public Console(int xLength,int yLength) throws IllegalArgumentException{
         this.matrix = new TheMatrix(xLength, yLength);
     }
-    
+
     //PUBLIC METHODS --------------------
     public void paintScreen(){
         int xLength = this.matrix.getXLength();
         int yLength = this.matrix.getYLength();
         String line;
-        
-        for(int x=0; x<xLength; x++){            
-            line = "";            
+
+        for(int x=0; x<xLength; x++){
+            line = "";
             for(int y=0; y<yLength; y++){
                 line += matrix.returnStrings()[x][y];
             }
             System.out.println(line);
         }
     }
-    
+
     public void clearScreen(){
+        //Prepares the screen for re painting (actual state of matrix does not change)
         for (int i=1; i<= this.matrix.getXLength(); i++){
             System.out.print(String.format("\033[%dA",1)); // Move up
-            System.out.print("\033[2K"); // Erase line content            
+            System.out.print("\033[2K"); // Erase line content
         }
     }
-    
+
+    public void paintAndClear(){
+        paintScreen();
+        try {
+            Thread.sleep( SLEEP_TIME );
+        } catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        clearScreen();
+    }
+
     public TheMatrix getMatrix(){
+        //Use this object to edit configuration of matrix
         return this.matrix;
     }
 }
--- TheMatrix.java
+++ TheMatrix.java
@@ -7,55 +7,88 @@ public class TheMatrix{
     public TheMatrix() throws IllegalArgumentException{
         this(10,10);
     }
-    
+
     public TheMatrix(int xLength, int yLength) throws IllegalArgumentException{
         if(this.isValidLength(xLength) && this.isValidLength(yLength)){
-            this.matrix = new boolean[xLength][yLength];            
+            this.matrix = new boolean[xLength][yLength];
         } else {
-            throw new IllegalArgumentException();            
+            throw new IllegalArgumentException("Matrix index cant be negative");
         }
 
     }
-    
+
    //PUBLIC METHODS --------------------
     public void setPixel(int x, int y, boolean state){
         if(this.isValidPixel(x,y)){
-            matrix[x][y] = state;            
+            matrix[x][y] = state;
         } else {
-            throw new IllegalArgumentException();   
+            throw new IllegalArgumentException("pixel outside matrix");
         }
     }
-    
+
     public void paintPixel(int x, int y){
-        this.setPixel(x, y, true);
+        if(this.isValidPixel(x,y)){
+            this.setPixel(x, y, true);
+        } else {
+            throw new IllegalArgumentException("pixel outside matrix");
+        }
     }
-    
+
+    public void paintPixel(int[][] coordinates){
+        for(int[] row : coordinates){
+            int x = row[0];
+            int y = row[1];
+            if(this.isValidPixel(x,y)){
+                this.setPixel(x, y, true);
+            } else {
+                throw new IllegalArgumentException("pixel outside matrix");
+            }
+        }
+    }
+
     public void deletePixel(int x, int y){
-        this. setPixel(x, y , false);
+        if(this.isValidPixel(x,y)){
+            this.setPixel(x, y, false);
+        } else {
+            throw new IllegalArgumentException("pixel outside matrix");
+        }
     }
-    
+
+    public void deletePixel(int[][] coordinates){
+        for(int[] row : coordinates){
+            int x = row[0];
+            int y = row[1];
+            if(this.isValidPixel(x,y)){
+                this.setPixel(x, y, false);
+            } else {
+                throw new IllegalArgumentException("pixel outside matrix");
+            }
+        }
+    }
+
+    //GETTERS METHODS --------------------
     public boolean getPixel(int x, int y){
         if(this.isValidPixel(x,y)){
-            return matrix[x][y];            
+            return matrix[x][y];
         } else {
-            throw new IllegalArgumentException();   
+            throw new IllegalArgumentException("pixel outside matrix");
         }
     }
-    
+
     public int getXLength(){
         return this.matrix.length;
     }
-    
+
     public int getYLength(){
         return this.matrix[0].length;
     }
-    
+
     public String[][] returnStrings(){
-        
+
         int xLength = this.getXLength();
         int yLength = this.getYLength();
         String[][] stringMatrix = new String[xLength][yLength];
-        
+
         for(int x=0; x<xLength; x++){
             for(int y=0; y<yLength; y++){
                 if(getPixel(x,y)){
@@ -66,20 +99,20 @@ public class TheMatrix{
                 }
             }
         }
-        
+
         return stringMatrix;
     }
-    
+
     //PRIVATE METHODS ------------------
     private boolean isValidLength(int length){
         return length>0;
     }
-    
+
     private boolean isValidPixel(int x, int y){
         if(x < this.matrix.length && x >= 0){
             return (y < this.matrix[x].length && y >= 0);
         } else {
             return false;
-        }          
+        }
     }
 }
